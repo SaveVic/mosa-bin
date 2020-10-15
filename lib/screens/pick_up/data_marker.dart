647334acf8fd448fc2e649d412019c64c719933b
waff <waffiqmaaroja1998@gmail.com>
2020-10-15 22:50:37 +0700

    implement maps

---
+++ lib/screens/pick_up/data_marker.dart
@@ -0,0 +1,62 @@
+const List<Map> dataMarker = [
+  {
+    'id': 'm1',
+    'lat': -7.7774087,
+    'long': 110.3742344,
+    'name': 'Bank sampah pertama',
+    'daerah': 'Sukadoi',
+    'pj': 'Pengurus',
+    'kontak': 08123548890,
+    'alamat': 'RT.13, Sukadoi, Bandung',
+    'sampah': [
+      {
+        'jenis': 'Taman',
+        'harga': 1000,
+      },
+      {
+        'jenis': 'Sisa Makanan',
+        'harga': 1500,
+      },
+    ],
+  },
+  {
+    'id': 'm2',
+    'lat': -7.7712857,
+    'long': 110.3974086,
+    'name': 'Bank sampah kedua',
+    'daerah': 'Sukaanda',
+    'pj': 'Mahasiswa',
+    'kontak': 08125678890,
+    'alamat': 'RT.14, Sukaanda, Bandung',
+    'sampah': [
+      {
+        'jenis': 'Taman',
+        'harga': 1500,
+      },
+      {
+        'jenis': 'Sisa Makanan',
+        'harga': 1000,
+      },
+    ],
+  },
+  {
+    'id': 'm3',
+    'lat': -7.7901646,
+    'long': 110.3887397,
+    'name': 'Bank sampah ketiga',
+    'daerah': 'Suka-suka',
+    'pj': 'Dosen',
+    'kontak': 08243438890,
+    'alamat': 'RT.14, Suka-suka, Bandung',
+    'sampah': [
+      {
+        'jenis': 'Taman',
+        'harga': 1200,
+      },
+      {
+        'jenis': 'Sisa Makanan',
+        'harga': 1800,
+      },
+    ],
+  }
+];
--- lib/screens/pick_up/pick_up.dart
+++ lib/screens/pick_up/pick_up.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mosa_bin/components/bottom_navbar.dart';
+import 'package:mosa_bin/components/custom_button.dart';
+import 'package:mosa_bin/components/custom_textfield.dart';
+
+import 'data_marker.dart';
 
 class PickUpPage extends StatefulWidget {
   @override
@@ -12,11 +17,47 @@ class PickUpPage extends StatefulWidget {
 
 class _PickUpPageState extends State<PickUpPage> {
   Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _markers = {};
 
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  final TextStyle _textStyle = TextStyle(
+    fontSize: 12.sp,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const LatLng _center = const LatLng(-7.7811505, 110.3791267);
+  bool _showInfo = false;
+  String _name;
+  String _area;
+  String _pj;
+  String _contact;
+  String _address;
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    for (Map m in dataMarker) {
+      _markers.add(
+        Marker(
+          markerId: m['id'],
+          position: LatLng(m['lat'], m['long']),
+          onTap: () {
+            setState(() {
+              _showInfo = true;
+              _name = m['name'];
+              _area = m['daerah'];
+              _pj = m['pj'];
+              _contact = m['kontak'];
+              _address = m['alamat'];
+            });
+          },
+        ),
+      );
+    }
   }
 
   @override
@@ -25,39 +66,108 @@ class _PickUpPageState extends State<PickUpPage> {
         designSize: Size(360, 640), allowFontScaling: false);
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Color(0xFF6A9923),
-          leading: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
+        appBar: buildAppBar(context),
+        body: Stack(
+          children: [
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 14.0,
+              ),
             ),
-          ),
-          title: Text(
-            'Jemput Sampah',
-            style: TextStyle(
-              fontSize: 14.sp,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
+            Container(
+              margin: EdgeInsets.fromLTRB(20.w, 20.h, 20.w, 0),
+              alignment: Alignment.topCenter,
+              child: CustomTextField(
+                width: double.infinity,
+                controller: null,
+                placeholder: 'Cari (Bank Sampah kota/kabupaten)',
+                suffixIcon: Icons.search,
+                onSuffixTap: () {},
+                radius: 10.w,
+                shadow: false,
+              ),
             ),
-          ),
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
+            (_showInfo)
+                ? Container(
+                    padding: EdgeInsets.all(20.w),
+                    alignment: Alignment.bottomCenter,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(_name, style: _textStyle),
+                        Text(_area, style: _textStyle),
+                        Text('Penanggung Jawab : $_pj', style: _textStyle),
+                        Text('Kontak : $_contact', style: _textStyle),
+                        Text('Alamat : $_address', style: _textStyle),
+                        SizedBox(height: 16.sp),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            CustomButton(
+                              color: Color(0xFFF9B915),
+                              width: 140.w,
+                              height: 30.h,
+                              text: Text(
+                                'Cek Harga',
+                                style: TextStyle(
+                                  fontSize: 12.sp,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            CustomButton(
+                              color: Color(0xFF6A9923),
+                              width: 140.w,
+                              height: 30.h,
+                              text: Text(
+                                'Jemput Sampah',
+                                style: TextStyle(
+                                  fontSize: 12.sp,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(),
+          ],
         ),
         bottomNavigationBar: MainBottomNavBar(
           pos: 0,
           onSelected: (id) {
             Navigator.pop(context, id);
           },
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0.0,
+      backgroundColor: Color(0xFF6A9923),
+      leading: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+      ),
+      title: Text(
+        'Jemput Sampah',
+        style: TextStyle(
+          fontSize: 14.sp,
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
         ),
       ),
     );
